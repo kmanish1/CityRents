f7aa40b2502587f99db69bb78fd5899d1feeb15f
thrishank <thrishankkalluru@gmail.com>
2024-06-25 13:38:50 +0530

    refactor code

--- UI/Hero.tsx
+++ UI/Hero.tsx
@@ -1,40 +1,67 @@
-// First Page
-
 import Link from "next/link";
+import CityIcon from "@/components/CityIcon";
+
+const data = [
+  { name: "Hyderabad", img: "/hyderabad.jpeg" },
+  { name: "Banglore", img: "/banglore.jpeg" },
+  { name: "Chennai", img: "/chennai.jpeg" },
+  { name: "Mumbai", img: "/mumbai.jpeg" },
+  { name: "Delhi", img: "/Delhi.jpeg" },
+];
+
+function City() {
+  return (
+    <div className="flex flex-wrap justify-center py-4 bg-white">
+      {data.map((item) => (
+        <div className="px-4">
+          <CityIcon name={item.name} img={item.img} />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function Hero() {
   return (
-    <section
-      className="relative w-full grid grid-cols-1 md:grid-cols-2 gap-4 items-center py-10"
-      // style={{ backgroundImage: 'url("/1.png")' }}
-    >
-      <div className="relative order-last md:order-first z-10 text-center max-w-2xl px-4 sm:px-6 lg:px-8 animate-fade-in">
-        <h1 className="text-2xl font-bold text-black sm:text-3xl md:text-4xl animate-fade-in-up">
-          Feeling Uncertain About Renting Out Your Property in India?
-        </h1>
-        <p className="mt-3 text-md text-gray-700 sm:mt-5 sm:text-xl animate-fade-in-up delay-100">
-          We can help. Sign up with Housewise and avail the best rental property management services in India.
-        </p>
-        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center animate-fade-in-up delay-200">
-          <Link
-            href="#"
-            className="mt-3 sm:mt-0 sm:ml-3 inline-flex items-center justify-center mx-8 px-8 py-3 border border-transparent text-base font-medium rounded-md text-primary bg-white hover:bg-gray-100 md:py-4 md:text-lg md:px-10"
-            prefetch={false}
-          >
-            Contact Us
-          </Link>
-          <Link
-            href="#"
-            className="mt-3 sm:mt-0 sm:ml-3 inline-flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-primary bg-white hover:bg-gray-100 md:py-4 md:text-lg md:px-10"
-            prefetch={false}
-          >
-            Explore Features
-          </Link>
+    <div>
+      <section
+        className="relative w-full grid grid-cols-1 md:grid-cols-2 gap-4 items-center py-10"
+        // style={{ backgroundImage: 'url("/1.png")' }}
+      >
+        <div className="relative order-last md:order-first z-10 text-center max-w-2xl px-4 sm:px-6 lg:px-8 animate-fade-in">
+          <h1 className="text-2xl font-bold text-black sm:text-3xl md:text-4xl animate-fade-in-up">
+            Feeling Uncertain About Renting Out Your Property in India?
+          </h1>
+          <p className="mt-3 text-md text-gray-700 sm:mt-5 sm:text-xl animate-fade-in-up delay-100">
+            We can help. Sign up with Housewise and avail the best rental
+            property management services in India.
+          </p>
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center animate-fade-in-up delay-200">
+            <Link
+              href="#"
+              className="mt-3 sm:mt-0 sm:ml-3 inline-flex items-center justify-center mx-8 px-8 py-3 border border-transparent text-base font-medium rounded-md text-primary bg-white hover:bg-gray-100 md:py-4 md:text-lg md:px-10"
+              prefetch={false}
+            >
+              Contact Us
+            </Link>
+            <Link
+              href="#"
+              className="mt-3 sm:mt-0 sm:ml-3 inline-flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-primary bg-white hover:bg-gray-100 md:py-4 md:text-lg md:px-10"
+              prefetch={false}
+            >
+              Explore Features
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="order-first md:order-last">
-        <img src="/apartment-rent-concept-illustration.png" width={700} height={700}></img>
-      </div>
-    </section>
+        <div className="order-first md:order-last">
+          <img
+            src="/apartment-rent-concept-illustration.png"
+            width={700}
+            height={700}
+          ></img>
+        </div>
+      </section>
+      <City />
+    </div>
   );
 }
--- UI/Services.tsx
+++ UI/Services.tsx
@@ -1,28 +1,60 @@
 import ServiceCard from "@/components/ServiceCard";
 
+function Choose() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 content-center py-5 mb-5 bg-white">
+      <div className="flex justify-center items-center">
+        <img src="/4295033-removebg-preview.png" height={500} width={500}></img>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h3 className=" text-xl md:text-2xl text-black font-semibold text-center pb-4">
+          Why should you choose us?
+        </h3>
+        <p className=" text-small p-2 pb-4">
+          Housewise is India’s fastest-growing rental property management
+          company. We provide transparent, seamless, and trustworthy Property
+          Management Solutions including rental property management services in
+          India to Non-resident Indians across the globe. From advertising,
+          tenant screening, rent collection to repairs, and beyond, Housewise
+          provides end-to-end property management services for Non-resident
+          Indians across the globe.
+        </p>
+        <div>
+          <button className="text-center rounded bg-blue-500 text-white px-4 py-2 ">
+            Learn More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Services() {
   return (
-    <div className="py-10">
-      <h1 className="text-2xl md:text-4xl text-black font-semibold text-center pb-6">
-        Our services include:
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <ServiceCard
-          icon="/mail.svg"
-          text="We provide end to end property management services in India for Non-Residents. From tenant search, monthly rent collection to repairs and periodic inspections, Housewise manages it all."
-          heading="End-to-End Property Management"
-        />
-        <ServiceCard
-          icon="/mail.svg"
-          text="Our rental property management services in India for Non-residents take care of everything from advertising your property, finding the right tenant, and support till tenant move-in."
-          heading="Rental Management"
-        />
-        <ServiceCard
-          icon="/mail.svg"
-          text="We provide customized property management services in India for Non-resident Indians."
-          heading="Custom Property Management"
-        />
+    <div>
+      <div className="py-10">
+        <h1 className="text-2xl md:text-4xl text-black font-semibold text-center pb-6">
+          Our services include:
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <ServiceCard
+            icon="/mail.svg"
+            text="We provide end to end property management services in India for Non-Residents. From tenant search, monthly rent collection to repairs and periodic inspections, Housewise manages it all."
+            heading="End-to-End Property Management"
+          />
+          <ServiceCard
+            icon="/mail.svg"
+            text="Our rental property management services in India for Non-residents take care of everything from advertising your property, finding the right tenant, and support till tenant move-in."
+            heading="Rental Management"
+          />
+          <ServiceCard
+            icon="/mail.svg"
+            text="We provide customized property management services in India for Non-resident Indians."
+            heading="Custom Property Management"
+          />
+        </div>
       </div>
+      <Choose />
     </div>
   );
 }

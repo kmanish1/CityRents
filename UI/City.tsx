debfcec3eb223d4fec87aa3cd68e7be7ac6e01a8
thrishank <thrishankkalluru@gmail.com>
2024-06-25 12:55:26 +0530

    add city icons

--- UI/City.tsx
+++ UI/City.tsx
@@ -2,13 +2,15 @@ import CityIcon from "@/components/CityIcon";
 
 const data = [
   { name: "Hyderabad", img: "/hyderabad.jpeg" },
-  { name: "Banglore", img: "" },
-  { name: "Chennai", img: "" },
+  { name: "Banglore", img: "/banglore.jpeg" },
+  { name: "Chennai", img: "/chennai.jpeg" },
+  { name: "Mumbai", img: "/mumbai.jpeg" },
+  { name: "Delhi", img: "/Delhi.jpeg" },
 ];
 
 export default function City() {
   return (
-    <div className="flex justify-center py-4 bg-white">
+    <div className="flex flex-wrap justify-center py-4 bg-white">
       {data.map((item) => (
         <div className="px-4">
           <CityIcon name={item.name} img={item.img} />

99c968b73a0d399f2bc68c464b04f7790afa2cde
kmanish1 <[kmanishreddy11@gmail.com]>
2024-06-25 11:08:57 +0530

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+### UI Components To add 
+Hero section -  https://ui.aceternity.com/components/flip-words
+Reviews - https://ui.aceternity.com/components/infinite-moving-cards
---
+++ UI/Facts.tsx
@@ -0,0 +1,16 @@
+import FactsCard from "@/components/FactsCard";
+
+
+export default function Facts() {
+  return (
+    <div className="pt-10 my-20 bg-gradient-to-r from-cyan-500 to-blue-500 ...">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        <FactsCard icon="/mail.svg" text="Properties" stats="4500"/>
+        <FactsCard icon="/mail.svg" text="Square-Feet" stats="5375000"/>
+        <FactsCard icon="/mail.svg" text="Happy Clients" stats="10000"/>
+        <FactsCard icon="/mail.svg" text="Cities" stats="13"/>
+        <FactsCard icon="/mail.svg" text="Countries" stats="33"/>
+      </div>
+    </div>
+  );
+}

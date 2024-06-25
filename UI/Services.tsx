0a7f1ca31486fb6a2dad4106146e4d9d122db046
thrishank <thrishankkalluru@gmail.com>
2024-06-25 11:29:02 +0530

    add files

--- UI/Services.tsx
+++ UI/Services.tsx
@@ -1,26 +1,28 @@
-import ServiceCard from "@/components/serviceCard";
-
-function Component() {
-  return (
-    <div>
-      <img />
-      <div>
-        <h1></h1>
-        <div></div>
-      </div>
-    </div>
-  );
-}
+import ServiceCard from "@/components/ServiceCard";
 
 export default function Services() {
   return (
-    <div>
-      <div>
-        <ServiceCard icon="" text="" heading=""/>
-        <ServiceCard icon="" text="" heading=""/>
-        <ServiceCard icon="" text="" heading=""/>
+    <div className="py-10">
+      <h1 className="text-2xl md:text-4xl text-black font-semibold text-center pb-6">
+        Our services include:
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <ServiceCard
+          icon="/icons8-property-50.png"
+          text="We provide end to end property management services in India for Non-Residents. From tenant search, monthly rent collection to repairs and periodic inspections, Housewise manages it all."
+          heading="End-to-End Property Management"
+        />
+        <ServiceCard
+          icon="/icons8-property-50.png"
+          text="Our rental property management services in India for Non-residents take care of everything from advertising your property, finding the right tenant, and support till tenant move-in."
+          heading="Rental Management"
+        />
+        <ServiceCard
+          icon="/icons8-property-50.png"
+          text="We provide customized property management services in India for Non-resident Indians."
+          heading="Custom Property Management"
+        />
       </div>
-      <Component />
     </div>
   );
 }
---
+++ components/FactsCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function FactsCard({
+  icon,
+  stats,
+  text,
+}: {
+  icon: string;
+  stats: string;
+  text: string;
+}) {
+  return (
+    <div
+      className={`text-center text-white hover:drop-shadow-md rounded-lg p-4 transition-transform transform hover:scale-102 duration-1000`}
+    >
+      <div className="flex items-center justify-center">
+        <img
+          src={icon}
+          className="w-[100px] text-center h-[100px] transition-transform transform hover:scale-105"
+          width={147}
+          height={147}
+        />
+      </div>
+      <div className=" my-6 text-center">
+        <h3 className=" text-xl md:text-2xl font-semibold text-center">
+          {stats}
+        </h3>
+        <p className=" text-small text-center p-2">{text}</p>
+      </div>
+    </div>
+  );
+}

7a06be21434a7d4b5002d0b5e367955d35c2ef46
thrishank <thrishankkalluru@gmail.com>
2024-06-25 23:53:54 +0530

    add blog files

---
+++ app/api/blog/route.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+mongoose.connect("");
+
+function GET() {
+  // get blogs
+}
+
+function POST() {
+  // add the new blog
+}
+
+function PUT() {
+  // update the blogs
+}
+
+function DELETE() {
+  // delete blog
+}
+
+export default function handler() {
+  // add the new blog
+}

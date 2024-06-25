48f0a7b8c2de0919f68e8988758e96308dc767a1
thrishank <thrishankkalluru@gmail.com>
2024-06-25 15:07:55 +0530

    add cards UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Facts from "@/UI/Facts";
 import Work from "@/UI/Work";
 import Benefits from "@/UI/Benefits";
 import Hero from "@/UI/Hero";
+import { Reviews } from "@/UI/reviews";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Facts />
       <Work />
       <Benefits />
+      <Reviews />
       <Contact />
     </div>
   );

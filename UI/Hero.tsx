75e1e3eb18b6de6180cd3ed2bc1f814bf22eebd3
thrishank <thrishankkalluru@gmail.com>
2024-06-25 14:55:07 +0530

    Fix navbar & form

--- UI/Hero.tsx
+++ UI/Hero.tsx
@@ -31,7 +31,7 @@ function City() {
 
 export default function Hero() {
   return (
-    <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8" id="#Home">
       <section
         className="relative w-full grid grid-cols-1 place-items-center md:grid-cols-2 gap-4 items-center px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8"
       >

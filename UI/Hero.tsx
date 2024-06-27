40a38797fcd73964ac287d2924ef3b2cb9857e6a
kmanish1 <kmanishreddy11@gmail.com>
2024-06-27 17:08:16 +0530

    made changes

--- UI/Hero.tsx
+++ UI/Hero.tsx
@@ -7,12 +7,13 @@ export default function Hero() {
   return (
     <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 dark:bg-white bg-black dark:bg-dot-black/[0.4] bg-dot-white/[0.4]" id="#Home">
       <section
-        className="relative w-full grid place-items-center grid-cols-2 gap-4 items-center pt-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl"
+        className="relative w-full pt-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl"
       >
-        <div className="relative order-last text-left md:order-first z-10 max-w-2xl  animate-fade-in">
+        <div className="relative text-left z-10 max-w-2xl  animate-fade-in">
           <h1 className=" text-black text-2xl sm:text-3xl md:text-4xl animate-fade-in-up z-0">
-            Rents Paid on Time <span className="font-bold inline-block my-4 text-3xl sm:text-4xl md:text-5xl">EVERYTIME !</span>
+            Rents Paid on Time
           </h1>
+          <h1 className="font-bold inline-block my-4 text-3xl sm:text-4xl md:text-5xl">EVERYTIME !</h1>
           {/* <p className="mt-3 text-md text-gray-700 sm:mt-5 sm:text-xl animate-fade-in-up delay-100">
             At CityRents, we redefine rental management through cutting-edge cloud-based technology. Our platform offers seamless solutions to landlords, tenants, and property managers, ensuring efficiency, transparency, and peace of mind.
           </p> */}

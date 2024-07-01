a644f6b442a1fb127d856aca8ef54f686724e9b1
kmanish1 <kmanishreddy11@gmail.com>
2024-07-01 19:02:49 +0530

    changes made

--- UI/Press.tsx
+++ UI/Press.tsx
@@ -1,18 +1,14 @@
-function Pressicon({ img }: { img: string}){
-    return(
-        <img src={img} width={100}></img>
-    )
-}
+import Pressicon from "@/components/Pressicon";
 
+const data = [
+    { img: "/news/newsimage1.jpeg" },
+    { img: "/news/newsimage2.jpeg" },
+    { img: "/news/newsimage3.jpeg" },
+    { img: "/news/newsimage4.jpeg" },
+    { img: "/news/newsimage5.jpeg" },
+    { img: "/news/newsimage6.jpeg" }
+  ];
 export default function Press(){
-    const data = [
-        { img: "/news/newsimage1.jpeg" },
-        { img: "/news/newsimage2.jpeg" },
-        { img: "/news/newsimage3.jpeg" },
-        { img: "/news/newsimage4.jpeg" },
-        { img: "/news/newsimage5.jpeg" },
-        { img: "/news/newsimage6.jpeg" },
-      ];
     return(
         <div className="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl bg-white py-6 mt-10">
             <h1 className="text-2xl md:text-4xl text-black font-semibold text-center pb-6 mb-4">
---
+++ components/Pressicon.tsx
@@ -0,0 +1,10 @@
+const Pressicon=({img}:{img:string})=>{
+
+    return(
+        <div>
+
+            <img src={img} width={100}></img>
+        </div>
+    );
+};
+export default Pressicon;

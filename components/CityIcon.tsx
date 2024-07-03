7d0cb465fd3ba145b2e94e52348be059164cf4d1
kmanish1 <kmanishreddy11@gmail.com>
2024-07-03 17:33:35 +0530

    coming soon and live now

--- components/CityIcon.tsx
+++ components/CityIcon.tsx
@@ -1,8 +1,8 @@
 import Image from "next/legacy/image";
 
-const MyComponent = ({ img, name }: { img: string; name: string }) => {
+const MyComponent = ({ img, name,available }: { img: string; name: string;available:string}) => {
   return (
-    <div className="group hover:bg-blue-100 flex flex-col items-center gap-4 p-4">
+    <div className="group hover:bg-blue-100 flex flex-col items-center gap-3 p-4 my-4">
       <div className="w-24 h-24 relative">
         <Image
           src={img}
@@ -14,6 +14,9 @@ const MyComponent = ({ img, name }: { img: string; name: string }) => {
       </div>
       <div className="text-medium text-black group-hover:text-blue-700 text-center">
         {name}
+      </div>
+      <div className="border-2 border-black p-2 rounded-full cursor-pointer">
+        {available}
       </div>
     </div>
   );
